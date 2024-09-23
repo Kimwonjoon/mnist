7aeb152b4c004d353631636987a0fe4bb9ace660
Kimwonjoon <kimpass189@gmail.com>
2024-09-23 11:30:27 +0900

    error solution

--- src/mnist/worker.py
+++ src/mnist/worker.py
@@ -22,27 +22,39 @@ def run():
     # STEP 2
     # RANDOM 으로 0 ~ 9 중 하나 값을 prediction_result 컬럼에 업데이트
     # 동시에 prediction_model, prediction_time 도 업데이트
-    pred = random.randint(0,9)
     ts = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d %H:%M:%S')
-    connection = get_conn()
-    with connection:
-        with connection.cursor() as cursor:
-            sql = f"UPDATE image_processing SET prediction_result={pred}, prediction_model='knn', prediction_time='{ts}' WHERE num={result['num']}"
-            cursor.execute(sql)
-        connection.commit()
+    if result == None:
+        ind = None
+    else:
+        ind = result['num']
+        pred = random.randint(0,9)
+        connection = get_conn()
+        with connection:
+            with connection.cursor() as cursor:
+                sql = f"UPDATE image_processing SET prediction_result={pred}, prediction_model='randint', prediction_time='{ts}' WHERE num={ind}"
+                cursor.execute(sql)
+            connection.commit()
     # STEP 3
     # LINE 으로 처리 결과 전송
     headers = {
         'Authorization': 'Bearer ' + os.getenv('LINE_NOTI_PATH', '0CexLLmrOvvWXwb21FLPvNXiwV3JKpqh7tcC9mjEmc2'),
     }
 
-    files = {
-        'message': (None, f"{result['num']}번째 이미지의 예측결과는 {pred}입니다."),
-    }
-
     if result == None:
         files = {
         'message': (None, f"업데이트 할 데이터가 없습니다."),
+    }
+        response = requests.post('https://notify-api.line.me/api/notify', headers=headers, files=files)
+
+        print(f"업데이트 할 데이터가 없습니다.")
+
+        return {
+            "prediction_time":ts,
+            "train_data_nth":ind,
+        }
+
+    files = {
+        'message': (None, f"{ind}번째 이미지의 예측결과는 {pred}입니다."),
     }
 
     response = requests.post('https://notify-api.line.me/api/notify', headers=headers, files=files)
