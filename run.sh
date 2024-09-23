0de234fab5507a93e4159523c1c11d2290823d68
Kimwonjoon <kimpass189@gmail.com>
2024-09-23 10:10:15 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #service cron start;uvicorn main:app --host 0.0.0.0 --port 8080 --reload
 
-echo DB_IP=172.17.0.1 >> /etc/environment;
-echo LINE_TOKEN=$LINE_NOTI_PATH >> /etc/environment;
+#echo DB_IP=172.17.0.1 >> /etc/environment;
+#echo LINE_TOKEN=$LINE_NOTI_PATH >> /etc/environment;
 service cron start;
 uvicorn main:app --host 0.0.0.0 --port 8080 --reload;
