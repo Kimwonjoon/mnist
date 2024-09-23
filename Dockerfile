8364fb30eb641bea0a2fbd5b62d01be82aef6b5a
Kimwonjoon <kimpass189@gmail.com>
2024-09-23 10:53:33 +0900

    ip 변경

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,16 @@ FROM python:3.11
 
 WORKDIR /code
 
-RUN apt update
-RUN apt install -y cron
-COPY ml-work-cronjob /etc/cron.d/ml-work-cronjob
-RUN crontab /etc/cron.d/ml-work-cronjob
-
 COPY src/mnist/main.py /code/
 COPY run.sh /code/run.sh
 
+RUN apt update
+RUN apt install -y cron
+RUN apt install -y vim
+COPY ml-work-cronjob /etc/cron.d/ml-work-cronjob
+RUN crontab /etc/cron.d/ml-work-cronjob
+
+RUN pip install --upgrade pip
 RUN pip install --no-cache-dir --upgrade git+https://github.com/Kimwonjoon/mnist.git@0.2/file
 
 CMD ["sh", "run.sh"]
--- src/mnist/db.py
+++ src/mnist/db.py
@@ -2,7 +2,13 @@ import pymysql.cursors
 import os
 
 def get_conn():
-  conn = pymysql.connect(host=os.getenv("DB_IP", "localhost"), port=int(os.getenv("MY_PORT", 53306)),
+#  conn = pymysql.connect(host=os.getenv("DB_IP", "localhost"),
+#                            port=int(os.getenv("MY_PORT", 53306)),
+#                            user = 'mnist', password = '1234',
+#                            database = 'mnistdb',
+#                            cursorclass=pymysql.cursors.DictCursor)
+  conn = pymysql.connect(host="172.17.0.1",
+                            port=int(os.getenv("MY_PORT", 53306)),
                             user = 'mnist', password = '1234',
                             database = 'mnistdb',
                             cursorclass=pymysql.cursors.DictCursor)
